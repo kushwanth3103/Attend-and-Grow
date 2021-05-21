d31c5a1d0a8cff1cb2bca859f6a28f64c8c961da
kushwanth3103 <bollepallikushwanthsai@gmail.com>
2021-05-22 00:25:55 +0530

    yah

--- public/events.js
+++ public/events.js
@@ -1,4 +1,17 @@
 $(document).ready(function(){
+    $(".but").click(function(){
+        console.log("hello")
+        $.ajax('/events_det',{
+            type:'POST',
+            dataType:'text',
+            data:$("but").val(),
+            success:function(data,status,xhr)
+            {
+                console.log("hurray")
+            }
+        
+        });
+    });
     $.ajax('/events',
     {
         type:'GET',
@@ -6,6 +19,7 @@ $(document).ready(function(){
         success:function(data1,status,xhr)
         {
             for(var i=0;i<data1.length;i++){
+                console.log(data1[i]._id)
                 var div="<div id=eventitem class=row>"
                 var image=""
                 var tags="<div id=desc class=col-sm-8><br>"
@@ -18,7 +32,7 @@ $(document).ready(function(){
                 var title="<br><p style=padding-top:10px>"+data1[i].name+"</p>"
                 var club="<p>"+data1[i].name+"</p>"
                 var desc="<p>"+data1[i].description+"</p></div>"
-                var button="<div class=col-sm-2 ><a><button style=background-color:#023E8A;color:#FFFFFF>View More</button></a></div></div>"
+                var button="<div class=col-sm-2 ><a><button style=background-color:#023E8A;color:#FFFFFF class='but' value='"+data1[i]._id+"'>View More</button></a></div></div>"
                 $("#eventslist").append(div+image+tags+title+club+desc+button);
             }
         },
