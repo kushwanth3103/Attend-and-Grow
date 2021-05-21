2c7593b29067aabd863d32fe3ca8282be95e4744
kushwanth3103 <bollepallikushwanthsai@gmail.com>
2021-05-21 11:46:04 +0530

    yesssssssss

--- public/events.js
+++ public/events.js
@@ -3,21 +3,21 @@ $(document).ready(function(){
     {
         type:'GET',
         dataType:'json',
-        success:function(data,status,xhr)
+        success:function(data1,status,xhr)
         {
-            for(var i=0;i<data.length;i++){
+            for(var i=0;i<data1.length;i++){
                 var div="<div id=eventitem class=row>"
-                var image="<img src='data:image/<%=data[i].img.contentType%>;base64,<%=data[i].img.data.toString('base64')%>' class='col-sm-2'>"
+                var image=""
                 var tags="<div id=desc class=col-sm-8><br>"
-                var arr_strings=data[i].tags
+                var arr_strings=data1[i].tags
                 var arr=arr_strings.split(',')
                 for(var j=0;j<arr.length;j++)
                 {
                     tags=tags+"<span class=eventtag>"+arr[j]+"</span>"
                 }
-                var title="<br><p style=padding-top:10px>"+data[i].name+"</p>"
-                var club="<p>"+data[i].name+"</p>"
-                var desc="<p>"+data[i].description+"</p></div>"
+                var title="<br><p style=padding-top:10px>"+data1[i].name+"</p>"
+                var club="<p>"+data1[i].name+"</p>"
+                var desc="<p>"+data1[i].description+"</p></div>"
                 var button="<div class=col-sm-2 ><a><button style=background-color:#023E8A;color:#FFFFFF>View More</button></a></div></div>"
                 $("#eventslist").append(div+image+tags+title+club+desc+button);
             }
